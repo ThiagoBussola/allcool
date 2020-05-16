f67495344d37007793642a3ef760d8429ce2950a
Bussola <thiagobussola@hotmail.com>
2020-05-16 17:29:39 +0000

    Mapeamento e teste entidade Address

---
+++ server/src/test/resources/sql/address/address.sql
@@ -0,0 +1,4 @@
+insert into file (id, url, filetype, filename, filedate)
+ values ('ce396aea-963e-11ea-bb37-0242ac130002', 'www.thiago.com.br', 'TipoTeste', 'ArquivoTeste', '2020-05-14 01:00:00');
+insert into file (id, url, filetype, filename, filedate)
+ values ('d33686e0-963e-11ea-bb37-0242ac130002', 'www.teste.com.br', 'tipoOutroTeste','OutroArquivoTeste',  '2020-06-12 00:00:01');
