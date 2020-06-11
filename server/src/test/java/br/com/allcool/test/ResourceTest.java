e9ff147dc510d47b0afefb2c257fc7fed1c1adca
MarceloASF <marceloasf1999@gmail.com>
2020-06-10 21:11:59 -0300

    inserts feitos do que foi mapeado no liquibase

--- server/src/test/java/br/com/allcool/test/ResourceTest.java
+++ server/src/test/java/br/com/allcool/test/ResourceTest.java
@@ -5,22 +5,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.test.annotation.Rollback;
-
 import org.springframework.test.context.ActiveProfiles;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@Inherited
-@Rollback
 @WebMvcTest
 @AutoConfigureMockMvc
 @EnableSpringDataWebSupport
