a3925024cfad72c6168ab14a3ed373595fecdd80
MarceloASF <marceloasf1999@gmail.com>
2020-06-17 23:57:45 -0300

    configuração básica necessária para imagens concluída

--- server/src/main/java/br/com/allcool/config/security/SecurityConfiguration.java
+++ server/src/main/java/br/com/allcool/config/security/SecurityConfiguration.java
@@ -59,6 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 			.and()
 			.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+				.antMatchers("/api/files/**").permitAll()
 			.anyRequest()
 			.authenticated();		
 			
