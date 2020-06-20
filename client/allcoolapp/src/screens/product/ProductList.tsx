427592dd4253d6b22304f6a24f257dbd5c7b353e
Bussola <thiagobussola@hotmail.com>
2020-06-20 23:30:08 +0000

    Tela de listagem

--- client/allcoolapp/src/screens/product/ProductList.tsx
+++ client/allcoolapp/src/screens/product/ProductList.tsx
@@ -3,7 +3,6 @@ import { View, Image, ScrollView, StyleSheet, FlatList } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
 import { Product } from '../../types';
-import requestExecutor from '../../service/AxiosService';
 
 type ProductListStackParamList = {
   Products: { userId: string } | undefined;
@@ -32,24 +31,22 @@ const ProductList: React.FC<Props> = ({ navigation, route: { params } }) => {
   const [products, setProducts] = useState<ProductDTO[]>([]);
   return (
     <>
-      <ScrollView>
-        <FlatList
-          data={products}
-          keyExtractor={(product) => product.id}
-          renderItem={({ item }) => (
-            <View style={{ alignItems: 'center', height: 50 }}>
-              <Image
-                style={{ width: 200, height: 215 }}
-                source={{
-                  uri:
-                    'https://i.pinimg.com/originals/53/9d/ca/539dca03d85f4e100f91a338bce0d246.png',
-                }}
-                resizeMode="cover"
-              />
-            </View>
-          )}
-        />
-      </ScrollView>
+      <FlatList
+        data={products}
+        keyExtractor={(product) => product.id}
+        renderItem={({ item }) => (
+          <View style={{ alignItems: 'center', height: 50 }}>
+            <Image
+              style={{ width: 200, height: 215 }}
+              source={{
+                uri:
+                  'https://i.pinimg.com/originals/53/9d/ca/539dca03d85f4e100f91a338bce0d246.png',
+              }}
+              resizeMode="cover"
+            />
+          </View>
+        )}
+      />
     </>
   );
 };
