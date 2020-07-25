bcc0e87f12c25298b4332351532d69a2ed6f98ff
eduardo.bonam <eduardo_bonam@hotmail.com>
2020-07-25 18:11:58 -0300

    Finalização da tela de Parceiros

--- client/allcoolapp/src/screens/partner/PartnerContainer.tsx
+++ client/allcoolapp/src/screens/partner/PartnerContainer.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
 import {
   PartnerContainerNavigationProp,
   PartnerContainerRouteProp,
 } from '../../navigation';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { boldTextStyles, textStyles, mainStyles } from '../../styles';
+import { Headline, Chip } from 'react-native-paper';
+import { PartnerService } from '../../service';
+import { PartnerViewDTO } from '../../types/dto';
+import { SnackbarNotification, SnackbarState } from '../../components';
+import { Rating } from 'react-native-ratings';
 
 type Props = {
   navigation: PartnerContainerNavigationProp;
@@ -13,22 +20,143 @@ type Props = {
 const PartnerContainer: React.FC<Props> = ({
   navigation,
   route: {
-    params: { userId },
+    params: { partnerId, userId },
   },
 }) => {
+  const [partner, setPartner] = useState<PartnerViewDTO>({});
+  const [snackbarState, setSnackbarState] = useState<SnackbarState>({
+    message: '',
+    visible: false,
+  });
+
+  useEffect(() => {
+    PartnerService.findById(partnerId)
+      .then(({ data }) => setPartner(data))
+      .catch(({ response }) =>
+        setSnackbarState({
+          message: response.data?.message || response.data,
+          visible: true,
+        })
+      );
+  }, [partnerId, userId]);
+
   return (
     <>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{`Bem vindo a tela de parceiros, usuário`}</Text>
-        <Text>{userId}</Text>
-        <View style={{ marginTop: 10 }}>
-          <Button
-            color="#ffbf00"
-            title="Visualizar Produtos"
-            onPress={() => navigation.navigate('Products')}
-          />
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{}}>
+          {!!partner.fileDTO?.id && (
+            <View style={{ alignSelf: 'center' }}>
+              <Image
+                style={{ width: 360, height: 240 }}
+                source={{
+                  uri: partner.fileDTO?.url,
+                }}
+                resizeMode="stretch"
+              />
+            </View>
+          )}
         </View>
-      </View>
+
+        <View style={mainStyles.container}>
+          <View style={{ alignItems: 'flex-start' }}>
+            <View style={{ marginTop: '3%' }}>
+              <Headline style={boldTextStyles}>{partner.name}</Headline>
+            </View>
+            <View>
+              <Rating
+                type="custom"
+                startingValue={partner.rating}
+                readonly
+                imageSize={20}
+              />
+            </View>
+          </View>
+
+          <View>
+            <Text style={textStyles}>{partner.address}</Text>
+          </View>
+
+          <View>
+            <Text style={textStyles}>{partner.locality}</Text>
+          </View>
+
+          <View>
+            <Text style={textStyles}>{`Fone: ${partner.phoneNumber}`}</Text>
+          </View>
+
+          <View>
+            <View style={{ marginTop: '2%' }}>
+              <Headline style={boldTextStyles}>Descrição</Headline>
+            </View>
+            <Text style={textStyles}>{partner.description}</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              marginTop: '5%',
+              flexWrap: 'nowrap',
+            }}
+          >
+            <View style={{ marginTop: '2%' }}>
+              <Headline style={boldTextStyles}>
+                Horário de Funcionamento
+              </Headline>
+            </View>
+            {partner.workingPeriodDTOList?.map((wp, index) => (
+              <View
+                key={index}
+                style={{
+                  margin: '1%',
+                  flexDirection: 'row',
+                }}
+              >
+                <View
+                  style={{ marginTop: '3%', alignItems: 'stretch', width: 85 }}
+                >
+                  <Text style={textStyles}>{`${wp.day}:`}</Text>
+                </View>
+                <View style={{ marginLeft: '5%' }}>
+                  <Chip
+                    accessibilityStates
+                    key={wp.id}
+                    mode="outlined"
+                    textStyle={{ color: 'black', fontSize: 14 }}
+                    selectedColor="black"
+                    style={{
+                      backgroundColor: '#ffbf00',
+                    }}
+                  >
+                    {wp.openingTime}
+                  </Chip>
+                </View>
+                <View style={{ marginLeft: '3%' }}>
+                  <Chip
+                    accessibilityStates
+                    key={wp.id}
+                    mode="outlined"
+                    textStyle={{ color: 'black', fontSize: 14 }}
+                    selectedColor="black"
+                    style={{
+                      backgroundColor: '#ffbf00',
+                    }}
+                  >
+                    {wp.closingTime}
+                  </Chip>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+      <SnackbarNotification
+        snackbarState={snackbarState}
+        dismissSnackbar={() =>
+          setSnackbarState({
+            message: '',
+            visible: false,
+          })
+        }
+      />
     </>
   );
 };
