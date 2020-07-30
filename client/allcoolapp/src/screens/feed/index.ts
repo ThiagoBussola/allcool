4d299ebc492053a36d400f373eeb0a187b7f7e3f
marcelo.filho <marceloasf1999@gmail.com>
2020-07-29 22:04:20 -0300

    iniciação concluída

---
+++ client/allcoolapp/src/screens/feed/index.ts
@@ -0,0 +1,2 @@
+export * from './PublicationList';
+export * from './PublicationView';
--- client/allcoolapp/src/screens/index.ts
+++ client/allcoolapp/src/screens/index.ts
@@ -2,3 +2,4 @@ export * from './login';
 export * from './partner';
 export * from './product';
 export * from './product-review';
+export * from './feed';
